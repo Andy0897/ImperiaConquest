bb7359654850a30992365a335f6cdfa6f273dd98
PC01-204 <andiiidim0@gmail.com>
2024-03-18 10:42:38 +0200

    Commit!

---
+++ src/main/java/com/example/ImperiaConquest/ImperiaConquestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ImperiaConquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperiaConquestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperiaConquestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ImperiaConquest/ImperiaConquestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ImperiaConquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImperiaConquestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
